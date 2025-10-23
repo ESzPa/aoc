6490d796b8e069f43a062c8ee43cd2ee19b03e25
ESzPa <peterszukk@gmail.com>
2025-10-23 22:51:52 +0200

    minimal cpp matrix class

---
+++ common/cpp/matrix.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+
+namespace aoc {
+    template <typename T>
+    class matrix {
+    private:
+        size_t rows_, cols_;
+        std::vector<std::vector<T>> matrix_;
+
+        inline void resize(size_t new_rows, size_t new_cols) {
+            matrix_.resize(new_rows, std::vector<T>(cols_, T()));
+
+            for(auto& row : matrix_) {
+                row.resize(new_cols, T());
+            }
+
+            rows_ = new_rows;
+            cols_ = new_cols;
+        }
+
+    public:
+        matrix(size_t rows, size_t cols) 
+            : rows_(rows), cols_(cols), matrix_(rows, std::vector<T>(cols, T())) {}
+
+        ~matrix() = default;
+
+        matrix(const matrix& other) = default;
+
+        T& at(size_t x, size_t y) {
+            return matrix_[x][y];
+        }
+
+        const T& at(size_t x, size_t y) const {
+            return matrix_[x][y];
+        }
+
+        T& operator()(size_t x, size_t y) {
+            if (x >= rows_ || y >= cols_) {
+                resize(std::max(rows_, x + 1), std::max(cols_, y + 1));
+            }
+
+            return &matrix_[x][y];
+        }
+
+        const T& operator()(size_t x, size_t y) const {
+            if (x >= rows_ || y >= cols_) {
+                throw std::out_of_range("Matrix index out of bounds");
+            }
+
+            return &matrix_[x][y];
+        }
+    };
+}
