335422944724a7eaff4a3e20f268d35664978831
ESzPa <peterszukk@gmail.com>
2026-03-24 10:01:32 +0100

    22/12.0 cpp

---
+++ 2022/12.0/main.cpp
@@ -0,0 +1,79 @@
+#include "common/cpp/file.hpp"
+#include "common/cpp/matrix.hpp"
+#include <print>
+#include <queue>
+
+size_t steps_a_to_b(const aoc::matrix<int>& mat, int sx, int sy,
+                    int dx, int dy) {
+    auto index = [&](int x, int y) { return y * mat.columns() + x; };
+    std::vector<bool> visited(mat.columns() * mat.rows(), false);
+    std::vector<size_t> dist(mat.columns() * mat.rows(), 0);
+    std::queue<std::pair<int, int>> q;
+
+    static constexpr int dir_x[] = {0, 0, -1, +1};
+    static constexpr int dir_y[] = {-1, +1, 0, 0};
+
+    q.push({sx, sy});
+    visited[index(sx, sy)] = true;
+    dist[index(sx, sy)] = 0;
+
+    int lastx, lasty;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        lastx = x;
+        lasty = y;
+        q.pop();
+
+        if (x == dx && y == dy) break;
+
+        for (size_t i = 0; i < 4; ++i) {
+            int nx = x + dir_x[i];
+            int ny = y + dir_y[i];
+            if (nx < 0 || ny < 0 || nx >= mat.columns() ||
+                ny >= mat.rows()) {
+                continue;
+            } else if (mat(ny, nx) > mat(y, x) + 1) {
+                continue;
+            } else if (visited[index(nx, ny)]) {
+                continue;
+            }
+
+            q.push({nx, ny});
+            visited[index(nx, ny)] = true;
+            dist[index(nx, ny)] = dist[index(x, y)] + 1;
+        }
+    }
+
+    return dist[index(lastx, lasty)];
+}
+
+int main(void) {
+    aoc::file f("input.txt");
+    aoc::matrix<int> mat(10, 10);
+
+    std::pair<int, int> start;
+    std::pair<int, int> dest;
+
+    size_t y = 0;
+    while (f.readline()) {
+        size_t x = 0;
+        for (char c : f.line) {
+            mat(y, x) = c - 96;
+            if (mat(y, x) == -13) {
+                start = {x, y};
+                mat(y, x) = 1;
+            } else if (mat(y, x) == -27) {
+                dest = {x, y};
+                mat(y, x) = 26;
+            }
+            ++x;
+        }
+        ++y;
+    }
+
+    size_t steps = steps_a_to_b(mat, start.first, start.second,
+                                dest.first, dest.second);
+    std::print("{}\n", steps);
+
+    return 0;
+}
