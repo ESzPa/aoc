ffe78b146f7ba41db9ad017573ff407d780c94cd
ESzPa <peterszukk@gmail.com>
2026-03-26 17:45:49 +0100

    matrix.hpp upgrade

--- common/cpp/matrix.hpp
+++ common/cpp/matrix.hpp
@@ -11,17 +11,17 @@ class matrix {
     std::vector<T> matrix_;
 
     constexpr inline void resize(size_t new_rows, size_t new_cols) {
-        if (new_rows == rows_ && new_cols == cols_) return;
+        if(new_rows == rows_ && new_cols == cols_)
+            return;
 
         std::vector<T> new_matrix(new_rows * new_cols, T());
 
         size_t min_rows = std::min(rows_, new_rows);
         size_t min_cols = std::min(cols_, new_cols);
 
-        for (size_t y = 0; y < min_rows; ++y) {
-            for (size_t x = 0; x < min_cols; ++x) {
-                new_matrix[y * new_cols + x] =
-                    std::move(matrix_[index_(y, x)]);
+        for(size_t y = 0; y < min_rows; ++y) {
+            for(size_t x = 0; x < min_cols; ++x) {
+                new_matrix[y * new_cols + x] = std::move(matrix_[index_(y, x)]);
             }
         }
 
@@ -30,13 +30,10 @@ class matrix {
         cols_ = new_cols;
     }
 
-    constexpr size_t index_(size_t y, size_t x) const noexcept {
-        return y * cols_ + x;
-    }
+    constexpr size_t index_(size_t y, size_t x) const noexcept { return y * cols_ + x; }
 
   public:
-    matrix(size_t rows, size_t cols)
-        : rows_(rows), cols_(cols), matrix_(cols_ * rows_) {}
+    matrix(size_t rows, size_t cols) : rows_(rows), cols_(cols), matrix_(cols_ * rows_) {}
 
     ~matrix() = default;
 
@@ -46,16 +43,16 @@ class matrix {
     constexpr size_t columns() const noexcept { return cols_; }
     constexpr size_t size() const noexcept { return cols_ * rows_; }
 
-    constexpr T& at(size_t y, size_t x) noexcept {
-        return matrix_[index_(y, x)];
-    }
+    constexpr T& at(size_t y, size_t x) noexcept { return matrix_[index_(y, x)]; }
 
-    constexpr const T& at(size_t y, size_t x) const noexcept {
-        return matrix_[index_(y, x)];
-    }
+    constexpr const T& at(size_t y, size_t x) const noexcept { return matrix_[index_(y, x)]; }
+
+    constexpr T& at_flat(size_t idx) noexcept { return matrix_[idx]; }
+
+    constexpr const T& at_flat(size_t idx) const noexcept { return matrix_[idx]; }
 
     constexpr T& operator()(size_t y, size_t x) {
-        if (y >= rows_ || x >= cols_) {
+        if(y >= rows_ || x >= cols_) {
             resize(std::max(rows_, y + 1), std::max(cols_, x + 1));
         }
 
@@ -63,25 +60,20 @@ class matrix {
     }
 
     constexpr const T& operator()(size_t y, size_t x) const {
-        if (y >= rows_ || x >= cols_) {
+        if(y >= rows_ || x >= cols_) {
             throw std::out_of_range("Matrix index out of bounds");
         }
 
         return matrix_[index_(y, x)];
     }
 
-    constexpr T& operator[](size_t y, size_t x) {
-        return (*this)(y, x);
-    }
+    constexpr T& operator[](size_t y, size_t x) { return (*this)(y, x); }
 
-    constexpr const T& operator[](size_t y, size_t x) const {
-        return (*this)(y, x);
-    }
+    constexpr const T& operator[](size_t y, size_t x) const { return (*this)(y, x); }
 
     template <typename Predicate>
     size_t count(Predicate predicate) {
-        return std::count_if(matrix_.begin(), matrix_.end(),
-                             predicate);
+        return std::count_if(matrix_.begin(), matrix_.end(), predicate);
     }
 };
 } // namespace aoc
